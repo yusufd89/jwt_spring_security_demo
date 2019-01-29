23a8905db10983426c34cedb6ce1f4a979de1bfb
yusufd89 <yusuf.d1989@gmail.com>
2019-01-29 17:31:22 +0300

    first commit

---
+++ src/main/java/com/demoapp2/security/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.demoapp2.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.demoapp2.model.security.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/demoapp2/security/service/JwtUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.demoapp2.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.demoapp2.model.security.User;
+import com.demoapp2.security.JwtUserFactory;
+import com.demoapp2.security.repository.UserRepository;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
+        } else {
+            return JwtUserFactory.create(user);
+        }
+    }
+}
